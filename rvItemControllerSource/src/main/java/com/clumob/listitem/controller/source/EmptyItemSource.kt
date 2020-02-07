3c90f1d21e98669a12bc12dc9dbcf65b3758b5a0
Prashant Rathore <prashant.rathore>
2020-02-07 21:49:46 +0530

    Converted to kotlin

---
+++ rvItemControllerSource/src/main/java/com/clumob/listitem/controller/source/EmptyItemSource.kt
@@ -0,0 +1,25 @@
+package com.clumob.listitem.controller.source
+
+/**
+ * Created by prashant.rathore on 17/12/18.
+ */
+class EmptyItemSource : ItemControllerSource<ItemController>() {
+    override fun onAttached() {}
+    override fun onItemAttached(position: Int) {}
+    override fun hasStableIds(): Boolean {
+        return false
+    }
+
+    override fun getItemPosition(item: ItemController): Int {
+        return 0
+    }
+
+    override fun getItemForPosition(position: Int): ItemController {
+        throw Exception("Its an EmptySource. Should not come here $position")
+    }
+
+    override fun onDetached() {}
+    override fun computeItemCount(): Int {
+        return 0
+    }
+}
